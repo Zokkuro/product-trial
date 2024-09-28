e4b913e0f3cad71f05effb3065f693c0297a6bd5
Delplanque Kévin <k.delplanque@gmail.com>
2024-09-28 12:24:56 +0200

    refactoring on ProductController

--- src/main/java/com/back/producttrial/controller/ProductController.java
+++ src/main/java/com/back/producttrial/controller/ProductController.java
@@ -10,36 +10,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping
 @RequiredArgsConstructor
 @Validated
 public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping
-    ResponseEntity<Long> addProduct(@RequestBody @Validated(ProductDTO.OnPost.class) ProductDTO product) {
-        return ResponseEntity.ok(productService.addProduct(product));
-    }
-
-    @GetMapping
+    @GetMapping("/products")
     ResponseEntity<List<ProductDTO>> getProducts() {
         return ResponseEntity.ok(productService.getAllProducts());
     }
 
-    @GetMapping("/{id}")
+    @PostMapping("/products")
+    ResponseEntity<Long> addProduct(@RequestBody @Validated(ProductDTO.OnPost.class) ProductDTO product) {
+        return ResponseEntity.ok(productService.addProduct(product));
+    }
+
+    @GetMapping("/products/{id}")
     ResponseEntity<ProductDTO> getProduct(@PathVariable("id") long productId) {
         ProductDTO result = productService.getProduct(productId);
         return result !=null ? ResponseEntity.ok(result) : ResponseEntity.notFound().build();
     }
 
-    @PatchMapping("/{id}")
+    @PatchMapping("/products/{id}")
     ResponseEntity<ProductDTO> updateProduct(@PathVariable("id") long productId, @RequestBody @Validated(ProductDTO.OnPatch.class) ProductDTO product) {
         ProductDTO result = productService.updateProduct(productId, product);
         return result !=null ? ResponseEntity.ok(result) : ResponseEntity.notFound().build();
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/products/{id}")
     ResponseEntity<Void> deleteProduct(@PathVariable("id") long productId) {
         productService.deleteProduct(productId);
         return ResponseEntity.noContent().build();
